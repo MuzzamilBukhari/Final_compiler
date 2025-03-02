04c49b90d9aae6ad6ac67765e528b65a09297321
Nabeel-hub685 <nabeelnaveed2004@gmail.com>
2025-03-03 02:03:43 +0500

    Updated folder with latest changes

--- LA.py
+++ LA.py
@@ -14,20 +14,101 @@ class Token:
         print(self.value_part)
         print(self.line_no)
 
-def word_break(file : str, index : int, line_no : int):
+
+# def word_break(file : str, index : int, line_no : int):
+    
+   
+
+   
+#     temp = "" 
+#     punctuatorsAndSingleOperators = {',', '(', ')', '{', '}', '[', ']', ':', ';', '?', '.', '+', '-', '*', '/', '%', '^'}
+
+#     while index < len(file):
+#         char = file[index]
+
+#         # If we already have some content in temp and a punctuator is encountered, return the word first
+#         if temp and char in punctuatorsAndSingleOperators:
+#             return temp, index, line_no  # Return the word first before handling punctuator
+
+#         # Handle newlines separately
+#         if char == '\n':
+#             line_no += 1
+#             index += 1
+#             if temp:
+#                 return temp, index, line_no  # Avoid returning an empty token
+#             continue  # Move to the next character if temp was empty
+
+#         # Handle spaces as word breakers
+#         if char == " ":
+#             index += 1
+#             if temp:
+#                 return temp, index, line_no  # Return the collected word
+#             continue  # Skip space without returning an empty token
+
+#         # If a punctuator is encountered
+#         if char in punctuatorsAndSingleOperators:
+#             # if temp:
+#             #     return temp, index, line_no  # Return the collected word first
+            
+#             index += 1
+#             return char, index, line_no  # Return the punctuator itself as a token
+        
+#         # Append normal characters to temp
+#         temp += char
+#         index += 1
+
+#     return temp, index, line_no #if temp else index, line_no  # Avoid returning an empty token at the end
+
+def word_break(file: str, index: int, line_no: int):
     temp = ""
-    for i in range(index, len(file)):
-        if (file[i] == '\n'):
+    punctuatorsAndSingleOperators = {',', '(', ')', '{', '}', '[', ']', ':', ';', '?', '.', '+', '-', '*', '/', '%', '^', '=', '<', '>'}
+    multi_char_operators = {"!=", "==", "<=", ">=", "+=", "-=", "*=", "/=", "%=", "^="}  # Multi-character operators set
+
+    while index < len(file):
+        char = file[index]
+
+        # If we already have some content in temp and a punctuator is encountered, return the word first
+        if temp and char in punctuatorsAndSingleOperators:
+            return temp, index, line_no  # Return the word before handling the operator
+
+        # Handle newlines separately
+        if char == '\n':
             line_no += 1
             index += 1
-            break
-        if(file[i] == " "): # word breaker
+            if temp:
+                return temp, index, line_no  # Avoid returning an empty token
+            continue  # Move to the next character if temp was empty
+
+        # Handle spaces as word breakers
+        if char == " ":
             index += 1
-            break
-        temp += file[i]
+            if temp:
+                return temp, index, line_no  # Return the collected word
+            continue  # Skip space without returning an empty token
+
+        # Handle multi-character operators (Lookahead logic)
+        if char in {"!", "=", "<", ">", "+", "-", "*", "/", "%", "^"}:
+            if index + 1 < len(file):  # Ensure we don't go out of bounds
+                next_char = file[index + 1]
+                potential_operator = char + next_char
+
+                if potential_operator in multi_char_operators:
+                    index += 2  # Move past both characters
+                    return potential_operator, index, line_no  # Return multi-character operator
+
+        # If a single-character operator is encountered
+        if char in punctuatorsAndSingleOperators:
+            index += 1
+            return char, index, line_no  # Return the operator itself as a token
+
+        # Append normal characters to temp
+        temp += char
         index += 1
 
-    return temp, index, line_no
+    return temp, index, line_no  # Avoid returning an empty token at the end
+
+
+
 
 def validate_word(word): # on working
     # print(word)
@@ -71,8 +152,9 @@ def LA (file):
         # print(index)
         class_part = validate_word(temp)
         token = Token(class_part, temp, line_no)
-        tokenSet.append(token)
-
+        # tokenSet.append(token)
+        # tokenSet.append({ 'value_part': token.value_part,'class_part': token.class_part, 'line_no':token.line_no})
+        tokenSet.append((token.value_part, token.class_part,token.line_no))
     return tokenSet
 
 
@@ -83,5 +165,6 @@ file.close()
 # print(src[0].find('\n'))
 # print(file)
 tokenSet = LA(src)
-for token in tokenSet:
-    token.print()
+# for token in tokenSet:
+#     token.print()
+print('tokenSet list=  ',tokenSet)
