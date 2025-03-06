f64489f411df17feafedddd51104abc49d1801d6
Nabeel-hub685 <nabeelnaveed2004@gmail.com>
2025-03-06 07:19:09 +0500

    changes made for strings

--- test.py
+++ test.py
@@ -2,5 +2,14 @@
 
 # print('let' in keywords.keywords)
 
-val='3433'
-print(val.isdigit())
+val='3433' 
+print(val.isdigit()) # is method se ham string me integers ko check krsaty hen!
+
+
+#  if char == '~':
+#             if temp:  
+#                 return temp, index, line_no  # Pehle temp return karo
+#             while index < len(file) and file[index] != '\n':
+#                 index += 1  # Comment ke characters skip karo
+#             index += 1  # Newline bhi skip kar do
+#             continue  # Agla character process karo
